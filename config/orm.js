fe68e8851060e8c80eabba0ded6f393f3d38d4e2
AKmach <akeimach@users.noreply.github.com>
2017-11-19 13:33:41 -0800

    init directory structure

---
+++ config/orm.js
@@ -0,0 +1,19 @@
+var connection = require("./connection.js");
+
+
+var orm = {
+    selectAll: function() {
+        return;
+    }
+
+    insertOne: function() {
+        return;
+    }
+
+    updateOne: function() {
+        return;
+    }
+};
+
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name) VALUES
+("bacon cheeseburger"), ("blue cheese burger"), ("BLT burger");
