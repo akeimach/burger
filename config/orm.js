44e1ab82fbcb9cad3006b5b84c3044ddb4911030
AKmach <akeimach@users.noreply.github.com>
2017-11-19 20:12:14 -0800

    working basic view

--- config/orm.js
+++ config/orm.js
@@ -2,16 +2,55 @@ var connection = require("./connection.js");
 
 
 var orm = {
-    selectAll: function() {
-        return;
+    selectAll: function(table, cb) {
+        var queryString = "SELECT * FROM " + table + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) throw err;
+            cb(result);
+        });
     },
 
-    insertOne: function() {
-        return;
+    insertOne: function(table, cols, vals, cb) {
+        var questions = [];
+        for (var i = 0; i < vals.length; i++) {
+            questions.push("?");
+        }
+        var queryString = "INSERT INTO " + table + "  (";
+        queryString += cols.toString();
+        queryString += ") VALUES (";
+        queryString += questions.toString();
+        queryString += ")";
+        connection.query(queryString, vals, function(err, result) {
+            if (err) throw err;
+            cb(result);
+        })
     },
 
-    updateOne: function() {
-        return;
+    updateOne: function(table, obj, condition, cb) {
+        var objects = [];
+        for (var key in obj) {
+            if (Object.hasOwnProperty.call(obj, key)) {
+                //if ((typeof(obj[key] === "string") && (obj[key].))
+                objects.push(key + "=" + obj[key]);
+            }
+        }
+        var queryString = "UPDATE " + table + " SET ";
+        queryString += objects.toString();
+        queryString += " WHERE ";
+        queryString += condition;
+        connection.query(queryString, function(err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+
+    deleteOne: function(table, condition, cb) {
+        var queryString = "DELETE FROM " + table + "WHERE ";
+        queryString += condition;
+        connection.query(queryString, function(err, result) {
+            if (err) throw err;
+            cb(result);
+        })
     }
 };
 
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,4 +4,48 @@ var burger = require("../models/burger.js");
 router = express.Router();
 
 
+///testing/////
+// var rootDir = { root: __dirname + '/..' };
+// router.get("/", function(req, res) {
+//     res.sendFile("/public/test.html", rootDir);
+// });
+///testing////
+
+
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        // Create object to send to handlebars
+        var hbsObject = {
+            burgers: data
+        };
+        res.render("index", hbsObject);
+    });
+});
+
+
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.name, req.body.devoured], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    burger.updateOne({ devoured: req.body.devoured }, condition, function(result) {
+        if (!result.affectedRows) return res.status(404).end();
+        res.status(200).end();
+    });
+});
+
+
+router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    burger.deleteOne(condition, function(result) {
+        if (!result.affectedRows) return res.status(404).end();
+        res.status(200).end();
+    });
+});
+
+
 module.exports = router;
